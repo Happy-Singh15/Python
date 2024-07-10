676dcf46c976a12d815a33f6275c52a0e2bf95d2
Happy Singh <solo.leveler15@gmail.com>
2024-07-10 14:06:42 +0530

    new

---
+++ data_Variables.py
@@ -0,0 +1,6 @@
+a=56
+b=65
+a=int (input("Value of a :"))
+b=int (input("Value of b :"))
+print(a,b)
+print(a+b)
---
+++ dictionary.py
@@ -0,0 +1,2 @@
+d={'name': 'nitish', 'college': 'bit', 'sem': 5, 'subjects': {'dsa': 80, 'english': 34, 'ds': 75}}
+print(d['subjects']['dsa'])
